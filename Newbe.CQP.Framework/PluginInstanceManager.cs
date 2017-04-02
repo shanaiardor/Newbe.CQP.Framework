5f73685db514c8f03c75d33643b31861126c5975
Newbe <472158246@qq.com>
2017-04-02 23:25:11 +0800

    插件加载成功

--- Newbe.CQP.Framework/PluginInstanceManager.cs
+++ Newbe.CQP.Framework/PluginInstanceManager.cs
@@ -1,23 +1,30 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using Newbe.CQP.Framework.Logging;
 
 namespace Newbe.CQP.Framework
 {
     public static class PluginInstanceManager
     {
+        private static readonly ILog Logger = LogProvider.GetLogger("PluginInstanceManager");
         private static PluginBase Instance { get; }
 
         static PluginInstanceManager()
         {
+            Logger.Info("开始加载插件");
             var pluginType =
                 AppDomain.CurrentDomain.GetAssemblies()
-                    .SelectMany(x => x.GetTypes().Where(a => a.IsAssignableFrom(typeof(PluginBase)) && !a.IsAbstract))
+                    .SelectMany(x => x.GetTypes().Where(a => typeof(PluginBase).IsAssignableFrom(a) && !a.IsAbstract))
                     .FirstOrDefault();
             if (pluginType == null)
             {
-                throw new Exception($"无法找到{typeof(PluginBase).Name}的实现类");
+                Logger.Error($"无法找到{typeof(PluginBase).Name}的实现类");
+                throw new Exception();
             }
             Instance = (PluginBase) Activator.CreateInstance(pluginType);
+            Logger.Info($"插件加载完毕，选取{pluginType.FullName}为插件实现类");
         }
 
         public static PluginBase GetInstance()
