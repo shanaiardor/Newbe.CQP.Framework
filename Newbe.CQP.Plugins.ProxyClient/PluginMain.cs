ac0809868b9e1319158128f06b98eb8920c0d1d1
Newbe <472158246@qq.com>
2017-04-03 10:14:28 +0800

    1.0.0 alpha 01

---
+++ Newbe.CQP.Plugins.ProxyClient/PluginMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newbe.CQP.Framework;
+
+namespace Newbe.CQP.Plugins.ProxyClient
+{
+    public class PluginMain : PluginBase
+    {
+        public override string AppId => "Newbe.CQP.Plugins.ProxyClient";
+
+        public override int ProcessPrivateMessage(int subType, int sendTime, long fromQQ, string msg, int font)
+        {
+            CoolQApi.SendPrivateMessage(fromQQ, "asduq");
+            return base.ProcessPrivateMessage(subType, sendTime, fromQQ, msg, font);
+        }
+    }
+}
