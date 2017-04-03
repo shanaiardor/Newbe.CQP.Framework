77317c26fef19cf62dac1ef85260fa87141c6b9a
Newbe <472158246@qq.com>
2017-04-03 08:50:41 +0800

    删除nlog

--- Newbe.CQP.Framework/PluginInstanceManager.cs
+++ Newbe.CQP.Framework/PluginInstanceManager.cs
@@ -9,19 +9,18 @@ namespace Newbe.CQP.Framework
 {
     public static class PluginInstanceManager
     {
-        private static readonly ILog Logger = LogProvider.GetLogger("PluginInstanceManager");
         private static IPluginBase Instance { get; }
         private static IContainer Container { get; }
 
         static PluginInstanceManager()
         {
-            Logger.Info("开始加载插件");
-
+            var logger = LogProvider.GetLogger("PluginInstanceManager");
+            logger.Info("开始加载插件");
             var builder = new ContainerBuilder();
             builder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies()).AsImplementedInterfaces().AsSelf();
             Container = builder.Build();
             Instance = Container.Resolve<IPluginBase>();
-            Logger.Info($"插件加载完毕，选取{Instance.GetType().FullName}为插件实现类");
+            logger.Info($"插件加载完毕，选取{Instance.GetType().FullName}为插件实现类");
         }
 
         public static IPluginBase GetInstance()
